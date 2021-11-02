96d80e9a3b3982d5fd029ce532bf5e455c814c29
SvetaDanielyan <sveta.danielyan.99@gmail.com>
2021-11-02 16:34:42 +0400

    Resets number to 0

--- src/Numbers.js
+++ src/Numbers.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./Numbers.css";
 
 const IncreaseingNumbers = () => {
   const [number, setNumber] = useState(0);
@@ -7,9 +8,16 @@ const IncreaseingNumbers = () => {
     setNumber(number + 1);
   };
 
+  const Reset = () => {
+    setNumber(0);
+  };
+
   return (
     <div>
-      <button onClick={IncreaseNumber}>Increase number</button>
+      <div className="Buttons">
+        <button onClick={IncreaseNumber}>Increase number</button>
+        <button onClick={Reset}>Reset to 0</button>
+      </div>
       <p>{number}</p>
     </div>
   );
