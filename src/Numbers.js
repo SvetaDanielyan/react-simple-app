f16f4e5d9fa2154e56c6bfa5087ca4b56a1e7685
SvetaDanielyan <sveta.danielyan.99@gmail.com>
2021-11-02 17:46:21 +0400

    Increases numbers randomly, keeps in localStorage

--- src/Numbers.js
+++ src/Numbers.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "./Numbers.css";
 
 const IncreaseingNumbers = () => {
-  const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(
+    parseInt(localStorage.getItem("number"))
+  );
+
+  useEffect(() => {
+    localStorage.setItem("number", number);
+  }, [number]);
 
   const IncreaseNumber = () => {
-    setNumber(number + 1);
+    setNumber(number + GenerateRandomNumber());
+  };
+
+  const GenerateRandomNumber = () => {
+    const min = 2;
+    const max = 10;
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+
+  const Reset = () => {
+    setNumber(0);
   };
 
   return (
     <div>
-      <button onClick={IncreaseNumber}>Increase number</button>
+      <div className="Buttons">
+        <button onClick={IncreaseNumber}>Increase number</button>
+        <button onClick={Reset}>Reset to 0</button>
+      </div>
       <p>{number}</p>
     </div>
   );
